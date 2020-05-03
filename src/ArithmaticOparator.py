5a31a665e4d1dd86b4d9694b79f2b10763c5c412
Sachinda <sachinda.n@arpicoinsurance.com>
2020-05-03 13:05:59 +0530

    learn basic arithmatic opration

---
+++ src/ArithmaticOparator.py
@@ -0,0 +1,33 @@
+x = 10;
+
+x = x + 3;
+print(x)
+
+x += 3
+print(f'assignment operator for addition {x}')
+
+x -= 3
+print(f'assignment operator for subtraction {x}')
+
+
+x *= 3
+print(f'assignment operator for multiply {x}')
+
+
+x /= 3
+print(f'assignment operator for division {x}')
+
+
+x **= 3
+print(f'assignment operator for exponentiation {x}')
+
+
+
+
+
+
+
+
+
+
+
