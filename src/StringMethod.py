651e63dc08217339a9653d1f4d2795e680a62015
Sachinda <sachinda.n@arpicoinsurance.com>
2020-05-03 12:49:49 +0530

    learn string method

---
+++ src/StringMethod.py
@@ -0,0 +1,21 @@
+text = "I can learn python"
+
+print(f'length : {len(text)}')
+
+print(f'uppercase : {text.upper()}')
+
+print(f'uppercase : {text.upper()}')
+
+print(f'title format : {text.title()}')
+
+print(f'find index : {text.find("n")}')
+
+print(f'replace : {text.replace("can" , "cannot")}')
+
+print(f'validate : {"can" in text}')
+
+
+
+
+
+
